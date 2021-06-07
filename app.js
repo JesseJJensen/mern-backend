a630012211f3f2923108e33f26725da713c97dc7
Jesse Jensen <81991370+JesseJJensen@users.noreply.github.com>
2021-06-07 10:12:27 -0400

    added schema

--- app.js
+++ app.js
@@ -1,34 +1,38 @@
 // Imports
-require('dotenv').config();
-const express = require('express');
-const app = express();
-const cors = require('cors');
-const passport = require('passport');
-const PORT = process.env.PORT || 8000;
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const passport = require('passport')
+const PORT = process.env.PORT || 8000
 
 // API
-const users = require('./api/users');
+const users = require('./api/users')
 
 // Middleware
-app.use(cors());
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+app.use(cors())
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
 
 // Initialize Passport and use config file
-app.use(passport.initialize());
+app.use(passport.initialize())
 
 // Home route
 app.get('/', (req, res) => {
-    res.status(200).json({ message: 'Smile, you are being watched by the Backend Engineering Team' });
-});
+  res
+    .status(200)
+    .json({
+      message: 'Smile, you are being watched by the Backend Engineering Team',
+    })
+})
 
 // Routes
-app.use('/api/users', users);
+app.use('/api/users', users)
 
 app.get('/*', (req, res) => {
-    res.status(404).json({ message: 'Data not found' });
-});
+  res.status(404).json({ message: 'Data not found' })
+})
 
 app.listen(PORT, () => {
-    console.log(`Server is listening 🎧 on port: ${PORT}`);
-});
+  console.log(`Server is listening 🎧 on port: ${PORT}`)
+})
